264c7c2b4716d688761105f0f4b18e4916d92fa8
konsbe <kons.be@outlook.com>
2022-02-22 23:00:29 +0200

    finish chess bag in rest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+countBlack=0
+countWhite=0
+
 board =[
                     [[1,1,"--"],[1,2,"--"],[1,3,"--"],[1,4,"--"],[1,5,"--"],[1,6,"--"],[1,7,"--"],[1,8,"--"]],
                     [[2,1,"--"],[2,2,"--"],[2,3,"--"],[2,4,"--"],[2,5,"--"],[2,6,"--"],[2,7,"--"],[2,8,"--"]],
@@ -8,34 +11,80 @@ board =[
                     [[6,1,"--"],[6,2,"--"],[6,3,"--"],[6,4,"--"],[6,5,"--"],[6,6,"--"],[6,7,"--"],[6,8,"--"]],
                     [[7,1,"--"],[7,2,"--"],[7,3,"--"],[7,4,"--"],[7,5,"--"],[7,6,"--"],[7,7,"--"],[7,8,"--"]],
                     [[8,1,"--"],[8,2,"--"],[8,3,"--"],[8,4,"--"],[8,5,"--"],[8,6,"--"],[8,7,"--"],[8,8,"--"]],
-        ]
+]
 
+for k in range(100):
+    wR=[0.0]
+    wB=[0.0]
+    bQ=[0.0]
+    chessBoard = board
+    if k == 1:
+        for l in range(len(chessBoard)):
+            print(chessBoard[l])
+        print("\n")
+    if k == 99:
+        for l in range(len(chessBoard)):
+            print(chessBoard[l])
+        print("\n")
+        print("\n")
+        print("\n")
 
-print("\n",board[1][3])
-print("\n",board[1][3][2])
+    wR= [random.randint(0, 7),random.randint(0, 7)]
 
-wR= [random.randint(0, 7),random.randint(0, 7)]
+    wB= [random.randint(0, 7),random.randint(0, 7)]
+    while wR == wB:
+        wB[0] = random.randint(0, 7)
+        wB[1] = random.randint(0, 7)
 
-wB= [random.randint(0, 7),random.randint(0, 7)]
-while wR == wB:
-    wN= [random.randint(1, 7),random.randint(0, 7)]
-
-bQ= [random.randint(0, 7),random.randint(0, 7)]
-while bQ == wR or bQ == wB:
     bQ= [random.randint(0, 7),random.randint(0, 7)]
+    while bQ == wR or bQ == wB:
+        bQ[0] = random.randint(0,7)
+        bQ[1] = random.randint(0,7)
 
-print(wR[0],wR[1])
-print(wB[0],wB[1])
-print(bQ[0],bQ[1])
-board[wR[0]][wR[1]][2] = "wR"
-board[wB[0]][wB[1]][2] = "wB"
-board[bQ[0]][bQ[1]][2] = "bQ"
-# print(board[wB[0]][1][wB[1]])
-print(wR[0]+wR[1])
+    if bQ == wR:
+        print("TRUE")
+    elif bQ==wB:
+        print("TRUE")
+    elif wR==bQ:
+        print("TRUE")
 
-for i in board:
-    print("\n \n",i)
+    chessBoard[wR[0]][wR[1]][2] = "wR"
+    chessBoard[wB[0]][wB[1]][2] = "wB"
+    chessBoard[bQ[0]][bQ[1]][2] = "bQ"
 
-random.randint(1,3)
+    if bQ[0]+bQ[1] == wR[0]+wR[1]:
+        countBlack += 1
+    elif bQ[0]-bQ[1] == wR[0]-wR[1]:
+        countBlack += 1
+    elif bQ[0] == wR[0]:
+        countWhite += 1
+        countBlack += 1
+    elif bQ[1] == wR[1]:
+        countWhite += 1
+        countBlack += 1
+    elif bQ[0]+bQ[1] == wB[0]+wB[1]:
+        countWhite += 1
+        countBlack += 1
+    elif bQ[0]-bQ[1] == wR[0]-wR[1]:
+        countWhite += 1
+        countBlack += 1
+
+
+print("points for black are:",countBlack)
+print("points for white are", countWhite)
+
+
+
+if countBlack > countWhite:
+    print("Black pawns win!")
+elif countWhite > countBlack:
+    print("Black pawns win!")
+else:
+    print("peace love and equality! :) ")
+
+
+
+
+
 
 
